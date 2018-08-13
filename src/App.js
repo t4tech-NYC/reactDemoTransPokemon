510ac08c9863dd3bfa492f25917f314357fd3c2d
Daly Barnett <dalybarnett@Dalys-MacBook-Air-26769.local>
2018-08-13 18:11:00 -0400

    basic boilerplate for starter app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import Search from './Search'
 
 class App extends Component {
   constructor(props){
     super(props)
 
     this.state = {
-      pokemonResults: 'torchic'
+     
     }
   }
 
   render() {
     return (
       <div className="App">
-        <h1>trans pokemon app!!!!</h1>
-        <Search />
+        <h1>header</h1>
       </div>
     );
   }
